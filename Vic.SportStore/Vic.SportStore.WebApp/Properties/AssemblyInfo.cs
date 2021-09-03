c927a3e2565ddc23bf9178d43a03487ffcec1a61
roy-lu <roylu8969@gmail.com>
2021-09-03 18:44:01 -0400

    [CHG] Add New WebApp Project"
    This reverts commit 328ce16afba830e0a63f56a7e1278d208cf0265b.

--- Vic.SportStore/Vic.SportStore.WebApp/Properties/AssemblyInfo.cs
+++ Vic.SportStore/Vic.SportStore.WebApp/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Vic.SportStore.WebApp")]
@@ -14,22 +14,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6962c69e-b5ae-4021-a812-589652e1800f")]
+[assembly: Guid("8bb2b505-4ce0-41b0-ba5b-478950899349")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers
+// You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
